0388ea5304b709c556a0d53954821db977333ef8
JU5TU5d <51639729+JU5TU5d@users.noreply.github.com>
2019-09-26 11:02:02 +0300

    Delete drawMenu.js

--- src/js/drawMenu.js
+++
@@ -1,8 +0,0 @@
-import menu from '../menu.json';
-import dishes from '../template/dish-card.hbs';
-
-const defs = {
-  menu: document.querySelector('.menu'),
-};
-const markup = menu.map(dish => dishes(dish)).join('');
-defs.menu.insertAdjacentHTML('afterbegin', markup);
